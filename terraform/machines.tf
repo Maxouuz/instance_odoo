dbdb3285cf203dca0f7e474cc6006eb30f5171a5
Stievenard Maxence <maxence.stievenard@univ-lille.fr>
2024-01-30 14:33:51 +0100

    changements de quelques variables pour que ça corrèle ensemble

--- terraform/machines.tf
+++ terraform/machines.tf
@@ -1,27 +1,27 @@
 resource "azurerm_public_ip" "pip_vm1_pip1" {
   name                = "pip-vm1-pip1-frc"
-  resource_group_name = azurerm_resource_group.rg_iut.name
-  location            = azurerm_resource_group.rg_iut.location
+  resource_group_name = azurerm_resource_group.rg_odoo.name
+  location            = azurerm_resource_group.rg_odoo.location
   allocation_method   = "Dynamic"
 }
 
 resource "azurerm_network_interface" "nic_vm1_nic1" {
   name                = "nic-vm1-nic1-frc"
-  resource_group_name = azurerm_resource_group.rg_iut.name
-  location            = azurerm_resource_group.rg_iut.location
+  resource_group_name = azurerm_resource_group.rg_odoo.name
+  location            = azurerm_resource_group.rg_odoo.location
 
   ip_configuration {
     name                          = "primary"
-    subnet_id                     = azurerm_subnet.snet_vnet1_iut1.id
+    subnet_id                     = azurerm_subnet.snet_vnet1_odoo1.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.pip_vm1_pip1.id
   }
 }
 
-resource "azurerm_linux_virtual_machine" "vm_iut_vm1" {
+resource "azurerm_linux_virtual_machine" "vm_odoo_odooServer" {
   name                            = "vm-iut-vm1-frc"
-  resource_group_name             = azurerm_resource_group.rg_iut.name
-  location                        = azurerm_resource_group.rg_iut.location
+  resource_group_name             = azurerm_resource_group.rg_odoo.name
+  location                        = azurerm_resource_group.rg_odoo.location
   size                            = "Standard_B1s"
   admin_username                  = "adminuser"
   disable_password_authentication = true
